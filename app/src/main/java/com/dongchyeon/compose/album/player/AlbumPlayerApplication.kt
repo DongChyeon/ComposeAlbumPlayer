15b95416dcae0e597842bb2fd70706185672437a
dongchyeon <kimdonghyun90766@gmail.com>
2026-01-07 22:25:47 +0900

    Refactor: ComponentCallbacks2를 통한 캐시 정리 제거
    LeastRecentlyUsedCacheEvictor가 캐시 초과 시 자동으로 LRU 삭제를 처리
    SimpleCache는 디스크 기반이므로 시스템 RAM 부족과 무관

--- app/src/main/java/com/dongchyeon/compose/album/player/AlbumPlayerApplication.kt
+++ app/src/main/java/com/dongchyeon/compose/album/player/AlbumPlayerApplication.kt
@@ -1,19 +1,7 @@
 package com.dongchyeon.compose.album.player
 
 import android.app.Application
-import android.content.ComponentCallbacks2
-import com.dongchyeon.core.media.cache.CacheManager
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class AlbumPlayerApplication : Application(), ComponentCallbacks2 {
-
-    @Inject
-    lateinit var cacheManager: CacheManager
-
-    override fun onTrimMemory(level: Int) {
-        super.onTrimMemory(level)
-        cacheManager.trimCacheIfNeeded()
-    }
-}
+class AlbumPlayerApplication : Application()
